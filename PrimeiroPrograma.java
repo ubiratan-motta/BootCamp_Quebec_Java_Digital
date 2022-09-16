4d5b63717b60ff1adacf53cee0c3bd4ae086359f
Ubiratan J. Motta Filho <mottaubiratan@gmail.com>
2022-09-16 19:41:26 -0300

    Add files via upload
    Iniciando a inclusão dos arquivos de estudos

---
+++ PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+package com.dio;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		int a = 2;
+		int b = 3;
+				System.out.println("Hello Word! " + (a + b));
+
+	}
+
+}
