10b73c547def639abcdd30f7642d3e72f4201a2f
Ubiratan J. Motta Filho <mottaubiratan@gmail.com>
2022-09-17 00:59:15 -0300

    Add files via upload

---
+++ PrimeiroPrograma.java
@@ -0,0 +1,10 @@
+package br.com.dio;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+//        int a = 5;
+//        int b = 6;
+//
+//        System.out.println("Hello World  " + (a+b));
+    }
+}
