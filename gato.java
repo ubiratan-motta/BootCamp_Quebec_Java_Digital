a664464884672f9a74f24d53a27b185f3a157d7f
Ubiratan J. Motta Filho <mottaubiratan@gmail.com>
2022-09-17 00:39:02 -0300

    Add files via upload
    Atividade realizada na primeira aula utilizando a IDE Eclipse

---
+++ gato.java
@@ -0,0 +1,71 @@
+package com.dio.model;
+
+import java.util.Objects;
+
+public class gato {
+
+	private String nome;
+	private String cor;
+	private Integer idade;
+
+	public gato() {
+	}
+
+	public gato(String nome, String cor, Integer idade) {
+		this.nome = nome;
+		this.cor = cor;
+		this.idade = idade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public Integer getIdade() {
+		return idade;
+	}
+
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cor, idade, nome);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		gato other = (gato) obj;
+		return Objects.equals(cor, other.cor) && Objects.equals(idade, other.idade) && Objects.equals(nome, other.nome);
+	}
+
+	@Override
+	public String toString() {
+		return "gato [nome=" + nome + ", cor=" + cor + ", idade=" + idade + "]";
+	}
+
+	/*
+	 * public gato(String nome, String cor, Integer idade) { this.nome = nome;
+	 * this.cor = cor; this.idade = idade; }
+	 */
+
+}
